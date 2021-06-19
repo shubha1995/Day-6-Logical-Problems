9dd09bb61ee09cb34455c3ecdd512e33703e17be
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-19 11:52:53 +0530

    Added: Fibonacci Series

---
+++ src/com/prgram/Utility.java
@@ -0,0 +1,15 @@
+package com.prgram;
+
+import java.util.Scanner;
+
+public class Utility {
+	private static final Scanner sc = new Scanner(System.in);
+	public static double getRandomDouble() {
+		return Math.random();
+	}
+	
+	public static int getUserSpecificInteger() {
+		return sc.nextInt();
+	}
+
+}
